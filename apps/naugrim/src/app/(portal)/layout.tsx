cd4634e4d5411f1e1dca070224d34754118e1daa
Pedro Duarte <pedrohlduarte@gmail.com>
2024-04-06 23:06:10 -0300

    feat: initial import search

--- apps/naugrim/src/app/(portal)/layout.tsx
+++ apps/naugrim/src/app/(portal)/layout.tsx
@@ -1,3 +1,6 @@
+import Image from 'next/image'
+import Link from 'next/link'
+
 import { Footer, ScrollToTop } from '@/components'
 import { Header } from '@/components/Header'
 
@@ -12,6 +15,20 @@ export default function PortalLayout({
       {children}
       <Footer />
       <ScrollToTop />
+      <div className="fixed w-15 h-15 z-50 bottom-20 right-5">
+        <Link
+          href="https://api.whatsapp.com/send?phone=5531992711521&text=Ol%C3%A1,%20Gestor!"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <Image
+            alt={'Icon whatsapp'}
+            src={'/images/icons/whats_button.png'}
+            width={79}
+            height={79}
+          />
+        </Link>
+      </div>
     </>
   )
 }
